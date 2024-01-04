f65891d6183ecb7aa691c9679bc9be22a27837d2
LUÍS PEDRO MAGALHÃES DA SILVA <8220025@estg.ipp.pt>
2024-01-04 23:58:11 +0000

    refactor: update code in order to have method to update Product.

--- e-commerce/Products/src/main/java/ecommerce/Repository/ProductsRepository.java
+++ e-commerce/Products/src/main/java/ecommerce/Repository/ProductsRepository.java
@@ -6,8 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProductsRepository extends JpaRepository<Product, Integer> {
     public List<Product> findAllByCategory(Category category);
     public List<Product> findAllByName(String name);
+
+    List<Product> findAll();
+
+    Optional<Product> findById(Integer id);
+
+    void delete(Product product);
+
 }
